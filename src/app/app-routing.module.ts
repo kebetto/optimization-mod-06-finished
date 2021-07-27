8eebcb2c9970a167d5c037f3f37c4812db0131f7
Kevin Kebe <kebettino@gmail.com>
2021-07-27 02:16:05 -0400

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: "", redirectTo: "/recipes", pathMatch: "full" },
+  {
+    path: "recipes",
+    loadChildren: "./recipes/recipes.module#RecipesModule"
+    // loadChildren: () =>
+    //   import("./recipes/recipes.module").then(m => m.RecipesModule)
+  },
+  {
+    path: "shopping-list",
+    loadChildren: "./shopping-list/shopping-list.module#ShoppingListModule"
+    // loadChildren: () =>
+    //   import("./shopping-list/shopping-list.module").then(
+    //     m => m.ShoppingListModule
+    //   )
+  },
+  {
+    path: "auth",
+    // loadChildren: () => import("./auth/auth.module").then(m => m.AuthModule)
+    loadChildren: "./auth/auth.module#AuthModule"
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIkey: "AIzaSyCH8UZ2oG3x6jGsxpPR6bDlNwXSOVlYO9w"
+};
